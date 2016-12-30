9ec79513eab31c668019c7ef343184460d541273
bagrat bagoyan <ext.bagrat.bagoyan@tieto.com>
2016-12-30 14:41:06 +0100

    styles

--- src/js/data/user.js
+++ src/js/data/user.js
@@ -1,18 +1,6 @@
 var User = {
 	username: "Bagrat",
 	balance: 500,
-	currencies: [
-		{
-			id: 1,
-			multiplier: 1.25,
-			dsp_name: "€"
-		},
-		{
-			id: 2,
-			multiplier: 1,
-			dsp_name: "$"
-		}
-	],
 	i_list: []
 }
 
